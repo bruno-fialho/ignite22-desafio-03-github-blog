e98d001ba313aed904306bfd1a0838b0a3488941
Bruno Fialho <brugerf@gmail.com>
2022-10-05 15:17:27 -0300

    feat: style config

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 1px ${(props) => props.theme.blue};
+  }
+
+  body {
+    background: ${(props) => props.theme['base-background']};
+    color: ${(props) => props.theme['base-text']};
+    --webkit-font-smoothing: antialiased;
+
+    overflow-x: hidden;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Nunito', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.6;
+  }
+
+  @media (max-width: 1200px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  @media (max-width: 768px) {
+    html {
+      font-size: 75%;
+    }
+  }
+`
