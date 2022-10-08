64d60d43516b60136d10f0664288006c29966ea5
Bruno Fialho <brugerf@gmail.com>
2022-10-08 13:33:03 -0300

    feat: eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { ThemeProvider } from "styled-components";
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/theme/default";
-import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
